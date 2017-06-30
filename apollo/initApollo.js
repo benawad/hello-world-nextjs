0295aa3a8c1e8eef2121d327673d4f13aaaf488d
ben awad <benawad97@gmail.com>
2017-06-29 22:07:19 -0500

    Setup subscriptions

--- apollo/initApollo.js
+++ apollo/initApollo.js
@@ -1,5 +1,12 @@
 import { ApolloClient, createNetworkInterface } from "react-apollo";
 import fetch from "isomorphic-fetch";
+import {
+  SubscriptionClient,
+  addGraphQLSubscriptions
+} from "subscriptions-transport-ws";
+
+const uri = "http://localhost:3000/graphql";
+const subscriptionUri = "ws://localhost:3000/subscriptions";
 
 let apolloClient = null;
 
@@ -8,17 +15,11 @@ if (!process.browser) {
   global.fetch = fetch;
 }
 
-function create(headers, initialState) {
+function create(headers, initialState, ni) {
   return new ApolloClient({
     initialState,
     ssrMode: !process.browser, // Disables forceFetch on the server (so queries are only run once)
-    networkInterface: createNetworkInterface({
-      uri: "http://localhost:3000/graphql", // Server URL (must be absolute)
-      opts: {
-        // Additional fetch() options like `credentials` or `headers`
-        credentials: "same-origin"
-      }
-    })
+    networkInterface: ni || createNetworkInterface({ uri })
   });
 }
 
@@ -31,7 +32,22 @@ export default function initApollo(headers, initialState = {}) {
 
   // Reuse client on the client-side
   if (!apolloClient) {
-    apolloClient = create(headers, initialState);
+    const wsClient = new SubscriptionClient(subscriptionUri, {
+      reconnect: true
+    });
+    const networkInterface = createNetworkInterface({
+      uri
+    });
+    const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+      networkInterface,
+      wsClient
+    );
+
+    apolloClient = create(
+      headers,
+      initialState,
+      networkInterfaceWithSubscriptions
+    );
   }
 
   return apolloClient;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { gql, graphql } from "react-apollo";
+import { compose, gql, graphql } from "react-apollo";
 import withData from "../apollo/withData";
 
 const query = gql`
@@ -10,15 +10,51 @@ const query = gql`
 }
 `;
 
-const index = ({ data: { allUsers } }) => (
-  <div>
-    Welcome to next.js!
-    <ul>
-      {allUsers.map(u => <li key={u.id}>{u.username}</li>)}
-    </ul>
-  </div>
-);
+const subscription = gql`
+subscription {
+  userAdded {
+    id
+    username
+  }
+}
+`;
 
-const GraphqlIndex = graphql(query)(index);
+class Index extends React.Component {
+  componentDidMount() {
+    this.props.data.subscribeToMore({
+      document: subscription,
+      updateQuery: (prev, { subscriptionData }) => {
+        /*
+          prev = {
+            anotherQuery: {}
+            allUsers: [bob1, bob2, ...],
+          }
+        */
+        if (!subscriptionData.data) {
+          return prev;
+        }
+        const newUser = subscriptionData.data.userAdded;
+        return {
+          ...prev,
+          allUsers: [newUser, ...prev.allUsers]
+        };
+      }
+    });
+  }
+
+  render() {
+    const { allUsers = [] } = this.props.data;
+    return (
+      <div>
+        Welcome to next.js!
+        <ul>
+          {allUsers.map(u => <li key={u.id}>{u.username}</li>)}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const GraphqlIndex = compose(graphql(subscription), graphql(query))(Index);
 
 export default withData(GraphqlIndex);
