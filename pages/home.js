ab3e722facc838bf7a23fd03e9c8238afecd83dc
ben awad <benawad97@gmail.com>
2017-07-04 14:55:46 -0500

    got localStorage working

--- pages/home.js
+++ pages/home.js
@@ -2,14 +2,21 @@ import React from 'react';
 
 export default class Home extends React.Component {
 
-  // static async getInitialProps(props) {
-  //   const { query } = props;
+  static async getInitialProps(props) {
+    const { query } = props;
+    const { token, refreshToken } = query;
+    if (localStorage) {
+      localStorage.setItem('token2', token);
+      localStorage.setItem('refreshToken2', refreshToken);
+    }
 
-  //   localStorage.setItem('token', query.token);
+    return {
+      query,
+    };
+  }
 
-  //   return {
-  //     token: query,
-  //   };
+  // componentDidMount() {
+  //   console.log("CLIENT SIDE");
   // }
 
   render() {
